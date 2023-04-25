02d355e8e358123a817ac158c7188d024ada0050
KMK_HM <kyun9151@gmail.com>
2023-04-25 11:31:09 +0900

    [17175] 피보나치는 지겨웡~

---
+++ baekjoon/230425/문제1/김명균.py
@@ -0,0 +1,15 @@
+"""
+Guess
+문제: https://www.acmicpc.net/problem/1248
+"""
+
+# n 입력
+n = int(input())
+
+# 부호 문자열 입력
+s = input()
+
+# 가능한 수
+nums = [i for i in range(-10, 0)] + [i for i in range(0, 11)]
+print(nums)
+
